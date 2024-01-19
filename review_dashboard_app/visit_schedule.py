a5924d7476499900a9690f9b34ef0a888c7ddb15
erikvw <ew2789@gmail.com>
2024-01-18 23:20:36 -0600

    update to work without model wrappers

--- review_dashboard_app/visit_schedule.py
+++ review_dashboard_app/visit_schedule.py
@@ -10,6 +10,7 @@ from edc_visit_schedule.visit import (
 )
 from edc_visit_schedule.visit_schedule import VisitSchedule
 
+from .consents import consent_v1
 from .lab_profiles import panel_one, panel_two
 
 app_label = "review_dashboard_app"
@@ -104,7 +105,7 @@ schedule = Schedule(
     name="schedule",
     onschedule_model=f"{app_label}.onschedule",
     offschedule_model=f"{app_label}.offschedule",
-    consent_model=f"{app_label}.subjectconsent",
+    consent_definitions=[consent_v1],
     appointment_model="edc_appointment.appointment",
 )
 
