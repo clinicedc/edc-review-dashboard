920864002afa7881457d003b647f50bf6f24f4d2
erikvw <ew2789@gmail.com>
2024-02-13 11:41:11 -0600

    tests

--- runtests.py
+++ runtests.py
@@ -31,6 +31,8 @@ project_settings = DefaultTestSettings(
         "django_crypto_fields.apps.AppConfig",
         "django_revision.apps.AppConfig",
         "edc_action_item.apps.AppConfig",
+        "edc_adverse_event.apps.AppConfig",
+        "adverse_event_app.apps.AppConfig",
         "edc_appointment.apps.AppConfig",
         "edc_auth.apps.AppConfig",
         "edc_consent.apps.AppConfig",
